3f215c1b1b48e47cd06c4cf9c056ce880f60e096
Tanya Labetskaya <tanya.labetskaya@wrenkitchens.com>
2020-04-03 15:02:31 +0300

    promise

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1004,3 +1004,114 @@ document.getElementById('smallTickets').addEventListener('click', () => {
         console.log(sumAllPages(500)); //5505
     })();
 });
+
+class Animal {
+    static type = 'ANIMAL';
+
+    constructor(options) {
+        this.name = options.name;
+        this.age = options.age;
+        this.hasTail = options.hasTail;
+    }
+
+    voice() {
+        console.log('i an animal');
+    }
+}
+
+class Cat extends Animal {
+    static type = 'CAT';
+
+    constructor(options) {
+        super(options);
+        this.color = options.color;
+    }
+
+    voice() {
+        console.log('i am cat');
+    }
+}
+
+const cat = new Cat({
+    name: 'Casper',
+    age: 5,
+    hasTail: true,
+    color: 'grey'
+});
+
+class Component {
+    constructor(selector) {
+        this.$el = document.querySelector(selector);
+    }
+
+    hide() {
+        this.$el.style.display = 'none'
+    }
+
+    show() {
+        this.$el.style.display = 'block'
+    }
+}
+
+class Box extends Component {
+    constructor(options) {
+        super(options.selector);
+
+        this.$el.style.width = this.$el.style.height = options.size + 'px';
+        this.$el.style.background = options.color;
+    }
+}
+
+const box1 = new Box({
+    selector: '#box1',
+    size: 100,
+    color: 'red'
+});
+
+const box2 = new Box({
+    selector: '#box2',
+    size: 150,
+    color: 'blue'
+});
+
+class Circle extends Box {
+    constructor(options) {
+        super(options);
+
+        this.$el.style.borderRadius = '50%';
+    }
+}
+
+const circle = new Circle({
+    selector: '#circle',
+    size: 50,
+    color: 'green'
+});
+
+(() => {
+    function uniqueValues(array) {
+        return [...new Set(array)];
+    }
+
+    console.log(uniqueValues([1, 1, 1, 2, 2, 3, 3, 3, 4]));
+})();
+
+// (() => {
+//     const cache = new WeakMap();
+//
+//     function cacheUser(user) {
+//         if (!cache.has(user)) {
+//             cache.set(user, Date.now());
+//
+//         }
+//         return cache.get(user);
+//     }
+//
+//     let lena = {name: 'Elena'};
+//     let alex = {name: 'Alex'};
+//
+//     lena = null;
+//
+//     cacheUser(lena); //false
+//     cacheUser(alex); //true
+// })();
